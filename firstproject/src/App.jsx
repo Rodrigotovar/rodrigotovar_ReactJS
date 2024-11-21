4ce5faa1e5ccb742dbdae06623329db0877becd9
Rodrigotovar <rodrigotovarpaura@gmail.com>
2024-11-21 16:56:44 -0600

    v

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -1,14 +1,23 @@
 import ItemListContainer from "./components/ItemListContainer/ItemListContainer"
 import NavBar from "./components/NavBar/NavBar"
-
+import {BrowerRouter, Routes, Route} from 'react-router-dom'
 function App() {
 
 
   return (
     <>
+    <BrowerRouter>
       <NavBar />
       <hr />
+      <Routes>
+        <Route path="/" element={<ItemListContainer />} />
+        <Route path="/category" element={<ItemListContainer />} />
+        <Route path="/detail" element={<ItemListContainer />} />
+        <Route path="*" element={<h1>404</h1>} />
+
+      </Routes>
       <ItemListContainer greeting="Bienvenido a tienda"/>
+      </BrowerRouter>
     </>
   )
 }
